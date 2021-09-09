c023e247a65b5a585e819d342d8c68cd5f2f3079
gingercook <vahitaras@gmail.com>
2021-09-08 23:46:26 -0400

    [200~MongooseServerSelectionError: connect ECONNREFUSED 127.0.0.1:21017

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 //jshint esversion:6
 
 const express = require("express");
+const mongoose = require('mongoose');
 const bodyParser = require("body-parser");
 const date = require(__dirname + "/date.js");
 
@@ -11,8 +12,32 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-const items = ["Buy Food", "Cook Food", "Eat Food"];
-const workItems = [];
+// const items = ["Buy Food", "Cook Food", "Eat Food"];
+// const workItems = [];
+
+main().catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://localhost:21017/todolistDB');
+}
+
+const itemsSchema = new mongoose.Schema({
+  name: String
+});
+
+const Item = mongoose.model('Item', itemsSchema);
+
+const item1 = new Item({
+  name: 'Buy Food'
+});
+
+const item2 = new Item({
+  name: 'Cook Food'
+});
+
+const item3 = new Item({
+  name: 'Eat Food'
+});
 
 app.get("/", function(req, res) {
 
@@ -21,6 +46,38 @@ const day = date.getDate();
   res.render("list", {listTitle: day, newListItems: items});
 
 });
+
+const defaultItems = [item1, item2, item3];
+
+const insertData = (defaultItems) => {
+  return new Promise((res, rej) => {
+    Item.insertMany(defaultItems, (err) => {
+      if(err){
+        console.error(err);
+        rej('Error!');
+      } else {
+        console.log('New entries are added to the database!');
+        res();
+      }
+    });
+  });
+};
+
+const asyncFunctions = async () => {
+  try{
+    await insertData(defaultItems);
+    console.log(items);
+
+    items.forEach(item => {
+      console.log(item.name)
+    });
+
+    mongoose.connection.close();
+
+  } catch(err) {};
+};
+
+asyncFunctions();
 
 app.post("/", function(req, res){
 
